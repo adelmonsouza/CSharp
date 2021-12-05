aa266a7b5ca4a133450087bb2d4724b4a9b95048
adelmonsouza <adelmonssilva@gmail.com>
2021-12-05 22:23:42 +0000

    Aulas 01 a 06 C#

---
+++ Aula06/Aula06.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Aula06{
+
+        static void Main(){
+
+            int n1, n2, n3;  
+            
+            
+            n1= 10; n2= 20; n3= 30;
+
+
+            Console.WriteLine("\nn1=\t{0}\nn2=\t{1}\nn3=\t{2}\n", n1, n2, n3 ); //Forma de indice
+        
+
+
+        }
+
+}
