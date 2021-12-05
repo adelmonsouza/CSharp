7db553efc68818b30047eb4bc22448833060398b
adelmonsouza <adelmonssilva@gmail.com>
2021-12-05 11:05:20 +0000

    Repositório Aulas C#

---
+++ Aula01_1/Curso_cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class aula01
+
+{
+        static void Main ()
+        {
+            Console.Write("Olá Mundo...\n");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+}
+
