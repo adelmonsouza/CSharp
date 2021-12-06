cb3dd7ff52a7f038b1c4a382abfefbe98f7b61c6
adelmonsouza <adelmonssilva@gmail.com>
2021-12-06 18:10:32 +0000

    Aulas 01 a 07 C#

--- Aula06/Aula06.cs
+++ Aula06/Aula06.cs
@@ -4,14 +4,30 @@ class Aula06{
 
         static void Main(){
 
-            int n1, n2, n3;  
+           /* int n1, n2, n3;  
             
             
             n1= 10; n2= 20; n3= 30;
 
 
-            Console.WriteLine("\nn1=\t{0}\nn2=\t{1}\nn3=\t{2}\n", n1, n2, n3 ); //Forma de indice
-        
+            Console.WriteLine("\nn1=\t{0}\nn2=\t{1}\nn3=\t{2}\n", n1, n2, n3 ); //Forma de indice */
+
+            double valorCompra = 5.50;
+            double valorVenda;
+            double lucro = 0.1;
+            string produto = "Pastel";
+
+            valorVenda = valorCompra + (valorCompra * lucro);
+
+            Console.WriteLine("Produto........:{0,15}",  produto);
+            Console.WriteLine("Val.Compra.....:{0,15:c}", valorCompra);
+            Console.WriteLine("Lucro. ........:{0,15:p}", lucro);
+            Console.WriteLine("Val.Venda......:{0,15:c}", valorVenda);
+
+            
+
+
+            
 
 
         }
---
+++ Aula07/Aula07.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+class Aula07{
+
+    static void Main(){
+
+        const string canal="Adelmon";
+        const double pi=3.1415;
+
+         Console.WriteLine("Canal: {0}\nPi: {1} ", canal, pi);
+
+    }
+
+}
